fe4e9d6ecb7e47f5773cbdeb24f7977fa9bc816d
diogomattos <diogomattos@users.noreply.github.com>
2017-11-21 11:58:44 -0200

    update the logic

--- extras/imc.py
+++ extras/imc.py
@@ -1,13 +1,19 @@
+print ("Descubra o seu IMC e como está sendo considerado o seu peso.")
 peso = int(input("Digite o seu peso (em kilos): "))
 altura = float(input("Digite a sua altura (em metros ex:1.56): "))
-gender = input("Digite o seu gênero (m ou f): ")
-imc = peso / (altura ** 2)
-if (imc >= 36) and (gender == "m") : print ("O seu IMC é:",imc,", considerado Obeso.")
-elif (imc > 32) and (gender == "m") and (imc < 36) : print ("O seu IMC é:",imc,", considerado Sobrepeso.")
-elif (imc > 26) and (gender == "m") and (imc < 32) : print ("O seu IMC é:",imc,", considerado Normal.")
-elif (imc < 20.7) and (gender == "m") : print ("O seu IMC é:",imc," considerado Abaixo da média.")
-if (imc >= 34) and (gender == "f") : print ("O seu IMC é:",imc,", considerado Obeso.")
-elif (imc > 30) and (gender == "f") and (imc < 34) : print ("O seu IMC é:",imc,", considerado Sobrepeso.")
-elif (imc > 24) and (gender == "f") and (imc < 30) : print ("O seu IMC é:",imc,", considerado Normal.")
-elif (imc < 19.1) and (gender == "f") : print ("O seu IMC é:",imc," considerado Abaixo da média.")
+gender = input("Digite o seu gênero (M ou F): ")
 
+cimc = peso / (altura ** 2)
+imc = round(cimc,2)
+
+if (imc < 20.7 and gender == "M") or (imc < 19.1 and gender == "F"):
+    print ("O seu IMC é",imc,"e é considerado Abaixo do Peso")
+elif (imc >= 20.7 and imc <= 26.4 and gender == "M") or (imc >= 19.1 and imc <= 25.8 and gender == "F"):
+    print ("O seu IMC é",imc,"e é considerado Peso ideal")
+elif (imc >= 26.5 and imc <= 27.8 and gender == "M") or (imc >= 25.9 and imc <= 27.3 and gender == "F"):
+    print ("O seu IMC é",imc,"e é considerado Pouco acima do peso")
+elif (imc >= 27.9 and imc <= 31.1 and gender == "M") or (imc >= 27.4 and imc <= 32.3 and gender == "F"):
+    print ("O seu IMC é",imc,"e é considerado Acima do peso")
+else :
+    print ("O seu IMC é",imc,"e é considerado Obesidade")
+    

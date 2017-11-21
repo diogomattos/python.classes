64dfa000c9b3e22af75890b58d76cdda803827be
diogomattos <diogomattos@users.noreply.github.com>
2017-11-21 10:35:22 -0200

    Delete segundos.py

--- segundos.py
+++
@@ -1,6 +0,0 @@
-segundosInput = int(input("Por favor, entre com o número de segundos que deseja converter:"))
-dias = segundosInput // 60 // 60 // 24
-horas = segundosInput // 60 // 60
-minutos = segundosInput // 60
-segundos = segundosInput-(minutos * 60)
-print (dias, "dias,",horas - dias * 24,"horas,",minutos - horas * 60,"minutos e", segundos,"segundos.")

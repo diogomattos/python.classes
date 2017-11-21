76ac0f0909276b9095264a6a0f67b3ba0c45bef2
diogomattos <diogomattos@users.noreply.github.com>
2017-11-21 10:36:28 -0200

    Delete ordenacao.py

--- ordenacao.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite um número: "))
-n3 = int(input("Digite um número: "))
-
-if (n2 > n1) and (n3 > n2):
-    print ("crescente")
-else:
-    print ("não está em ordem crescente")
